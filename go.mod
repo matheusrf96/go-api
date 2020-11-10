ec75ed5b84efbb579021a28d7a437209e34f81ad
Matheus Rodrigues <matheusrf96@gmail.com>
2020-11-10 20:29:12 -0300

    Implement postgres connection

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/matheusrf96/api
 
 go 1.15
+
+require (
+	github.com/bmizerany/pq v0.0.0-20131128184720-da2b95e392c1
+	github.com/lib/pq v1.8.0
+)
--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 
 	"github.com/matheusrf96/api/controller"
+	"github.com/matheusrf96/api/model"
 )
 
 func main() {
 	fmt.Println("Serving on 0.0.0.0:3000")
 
 	mux := controller.Register()
+
+	db := model.Connect()
+	defer db.Close()
 
 	http.ListenAndServe("0.0.0.0:3000", mux)
 }
